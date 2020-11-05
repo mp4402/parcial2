93123e6d22cd17da288a6c99e00947a002e900ba
Mario Enrique Pisquiy Gómez <69279232+mp4402@users.noreply.github.com>
2020-11-05 08:11:03 -0600

    Add files via upload

--- parcial2/server.py
+++ parcial2/server.py
@@ -11,20 +11,22 @@ app = Flask(__name__)
 output = [""]
 
 def reverse(cadena):
+    nCadena = ""
     for n in cadena:
         nCadena = n + nCadena
     output[0] = nCadena
 
 def longitud(cadena):
     largoCadena = len(cadena)
+    str(largoCadena)
     output.append(largoCadena)
 
 def vowelsConsonants(cadena):
     vocal = 0
     consonante = 0
     carAsc = 0
-    vocales = [65, 69, 73, 79, 85, 97, 101, 105, 111, 117]
-    consonantes = [66, 67, 68, 70, 71, 72, 73, 74, 75, 76, 77, 78, 80, 81, 82, 83, 84, 86, 87, 88, 89, 90, 98, 99, 100, 102, 103, 104, 106, 107, 108, 109, 110, 112, 113, 114, 115, 116, 118, 119, 120, 121, 122]
+    vocales = ["65", "69", "73", "79", "85", "97", "101", "105", "111", "117"]
+    consonantes = ["66", "67", "68", "70", "71", "72", "73", "74", "75", "76", "77", "78", "80", "81", "82", "83", "84", "86", "87", "88", "89", "90", "98", "99", "100", "102", "103", "104", "106", "107", "108", "109", "110", "112", "113", "114", "115", "116", "118", "119", "120", "121", "122"]
     for n in cadena:
         carAsc = ord(n)
         for m in vocales:
@@ -34,7 +36,8 @@ def vowelsConsonants(cadena):
         for m in consonante:
             if carAsc == m:
                 consonante += 1
-    
+    str(vocales)
+    str(consonante)
     output.append(vocales)
     output.append(consonante)
 
@@ -42,35 +45,51 @@ def upper(cadena):
     upCadena = cadena.upper()
     output.append(upCadena)
 
-def lower():
-    pass
+def lower(cadena):
+    lowCadena = cadena.lower()
+    output.append(lowCadena)
 
-def upDown():
-    pass
+def upDown(cadena):
+    nuevaCadena = ""
+    contador = 0
+    for n in cadena:
+        contador += 1
+        if (contador % 2 != 0):
+            n = n.upper()
+        nuevaCadena += n
+    output.append(nuevaCadena)
 
-def naive():
-    pass
+def naive(cadena):
+    nuevaCadena = ""
+    for n in cadena:
+        if n == "a" or n == "A":
+            n = "@"
+        elif n == "e" or n == "E":
+            n = "3"
+        elif n == "i" or n == "I":
+            n = "!"
+        elif n == "o" or n == "O":
+            n = "0"
+        elif n == "u" or n == "U":
+            n = ")"
+        else:
+            n = n
+        nuevaCadena += n
+    output.append(nuevaCadena)
 
 @app.route("/", methods=['GET','POST'])
 def index():
     if request.method == 'POST':
         _input = request.form['input']
-        return redirect(url_for("index"))
+        reverse(_input)
+        longitud(_input)
+        vowelsConsonants(_input)
+        upper(_input)
+        lower(_input)
+        upDown(_input)
+        naive(_input)
+        return redirect(url_for("index"), my_list = output)
     template = env.get_template("index.html")
-    return template.render()
-
-@app.route("/crear", methods=['GET', 'POST'])
-def crear():
-    if request.method == 'POST':
-        _id = request.form["id"]
-        _type = request.form["type"]
-        _name = request.form["name"]
-        _quantity = request.form["quantity"]
-        
-        my_json["data"].append({"id":_id,"type":_type,"name":_name,"quantity":_quantity})
-        return redirect(url_for("index"))
-
-    template = env.get_template("form.html")
     return template.render()
 
 if __name__ == "__main__":
